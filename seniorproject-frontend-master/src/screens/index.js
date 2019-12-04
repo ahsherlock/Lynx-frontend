b75291914660bebe6de7bed410d48614ea141f48
Alec Sherlock <38014153+ahsherlock@users.noreply.github.com>
2019-12-04 15:41:10 -0800

    Add files via upload

---
+++ seniorproject-frontend-master/src/screens/index.js
@@ -0,0 +1,5 @@
+export HomeScreen from './home/HomeScreen';
+export MapViewScreen from './home/MapViewScreen';
+export NearbyScreen from './home/NearbyScreen';
+export SettingsScreen from './settings/SettingsScreen';
+export FavoritesScreen from './home/FavoritesScreen';
